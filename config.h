421ade8fefc0a0b402746ed7fdb3828fdba8327a
Ren <73724137+pra1rie@users.noreply.github.com>
2023-12-30 11:57:18 +0100

    uwu owo

--- config.h
+++ config.h
@@ -214,8 +214,8 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 
-	{ TERMMOD,              XK_Up,          kscrollup,      {.i =  1} },
-	{ TERMMOD,              XK_Down,        kscrolldown,    {.i =  1} },
+	{ ShiftMask,            XK_Up,          kscrollup,      {.i =  1} },
+	{ ShiftMask,            XK_Down,        kscrolldown,    {.i =  1} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
